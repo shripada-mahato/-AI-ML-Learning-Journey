2f1f861eea8ec99d8dec6799637f2d88e6a03bae
Shripada Mahato <mahatoshripada692@gmail.com>
2026-07-26 17:01:06 +0530

    remove some folder from this file

--- NUMPY/hello.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-count = 0
-arr = [10,20,30]
-for i in arr:
-    count += 1
-    print(i)
-    print(count)
--- PANDAS/printdata.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-result = pd.read_csv("archive/ecommerce_dataset_updated.csv")
-print_rows = result["Category","Payment_Method","Purchase_Date","Discount (%)"]
-print(print_rows.head().to_string())
